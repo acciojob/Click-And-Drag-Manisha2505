7bb8f58d4a1a48263daa70035a22883ec40f2953
Manisha2505 <manishachandra.25@gmail.com>
2025-02-03 19:56:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your code here.
+ let draggedImage = null;
+    function allowDrop(event){
+        event.preventDefault();
+    }
+    function drag(event){
+        draggedImage = event.target;
+    }
+
+    function drop(event){
+        event.preventDefault();
+        const targetContainer = event.currentTarget;
+
+       if(targetContainer !== draggedImage.parentElement){
+        const targetImage = targetContainer.querySelector('img');
+
+        const tempSrc = targetImage.src;
+        targetImage.src = draggedImage.src;
+        draggedImage.src = tempSrc;
+       }
+    }
